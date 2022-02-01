841f007d1633cef599965686ea8b1d067b425489
Vladimir Timotijevic <84339808+TimotijevicVlada@users.noreply.github.com>
2022-02-01 14:31:02 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style/App.css';
+
+function App() {
+
+
+
+  
+  return (
+    <div className="App">
+      <h1>Hello Type Script</h1>
+    </div>
+  );
+}
+
+export default App;
